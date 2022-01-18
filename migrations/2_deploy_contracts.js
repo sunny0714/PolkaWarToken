d545744d6a9ed0d49607da3d16cb1ac809858c1a
sunny0714 <sun2020victory@gmail.com>
2022-01-18 07:59:15 +0200

    fix big number

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
+const BigNumber = require('bignumber.js');
+
 const PolkaWarToken = artifacts.require("PolkaWarToken");
 
 module.exports = async function (deployer) {
-  await deployer.deploy(PolkaWarToken, 10000);
+  await deployer.deploy(PolkaWarToken, new BigNumber(100000000000000000000000000));
 };
